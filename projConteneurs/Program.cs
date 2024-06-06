c2a10c081d23248303b86842a465e61b174ce737
Christ ASSANA <christassana@gmail.com>
2024-06-06 22:43:12 +0200

    Ajoutez des fichiers projet.

---
+++ projConteneurs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class MainProgram
+{
+    public static void Main()
+    {
+        int[] tab = new int[10];
+        string x;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            Console.WriteLine("Veuillez renseigner un entier. ");
+            x = Console.ReadLine();
+            tab[i] = Convert.ToInt32(x); 
+        }
+        
+        Console.WriteLine("Voici les valeurs de otre tableau. ");
+
+        for (int i = 0; i < tab.Length; i++)
+        {
+            Console.WriteLine(tab[i]);
+        }
+    }
+}
